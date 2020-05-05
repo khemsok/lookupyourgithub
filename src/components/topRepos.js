ef8d0e941fb11eee9cacf5c3d6a499aca9138656
Khem Sok <khemsok97@gmail.com>
2020-05-05 10:47:37 -0400

    Fixed color of fork in dark mode

--- src/components/topRepos.js
+++ src/components/topRepos.js
@@ -19,11 +19,11 @@ import Tooltip from "@material-ui/core/Tooltip";
 // MUI Icon
 import BookmarksIcon from "@material-ui/icons/Bookmarks";
 import StarIcon from "@material-ui/icons/Star";
-import RestaurantIcon from "@material-ui/icons/Restaurant";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 // Icon
 import gitfork from "../repo-forked.svg";
+import Octicon, { getIconByName } from "@primer/octicons-react";
 
 const useStyles = makeStyles({
   paperStyled: {
@@ -106,55 +106,68 @@ export default function TopRepos(props) {
                   {checkDescriptionLength(element.description)}
                 </Typography>
               </div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <div>
+                  <div style={{ display: "inline" }}>
+                    <span>
+                      <FiberManualRecordIcon
+                        style={{
+                          color: selectLanguageColor(element.language),
+                          verticalAlign: "bottom",
+                          marginRight: "5px",
+                        }}
+                      />
+                    </span>
+                    <Typography display="inline">{element.language}</Typography>
+                  </div>
 
-              <Typography display="inline">
-                <span>
-                  <FiberManualRecordIcon
-                    style={{
-                      color: selectLanguageColor(element.language),
-                      verticalAlign: "sub",
-                      marginRight: "5px",
-                      fontSize: "1.25em",
-                    }}
-                  />
-                </span>
-                {element.language}
-              </Typography>
+                  <div style={{ display: "inline" }}>
+                    <span>
+                      <Tooltip title="Stars" placement="top">
+                        <StarIcon
+                          style={{
+                            verticalAlign: "sub",
+                            fontSize: "1.25em",
+                            marginLeft: "15px",
+                            marginRight: "5px",
+                          }}
+                        />
+                      </Tooltip>
+                    </span>
+                    <Typography display="inline">
+                      {element.stargazers_count}
+                    </Typography>
+                  </div>
 
-              <Typography display="inline" style={{ float: "right" }}>
-                {numberWithCommas(element.size)} KB
-              </Typography>
-              <Typography display="inline">
-                <span>
-                  <Tooltip title="Stars" placement="top">
-                    <StarIcon
-                      style={{
-                        verticalAlign: "sub",
-                        fontSize: "1.25em",
-                        marginLeft: "15px",
-                        marginRight: "5px",
-                      }}
-                    />
-                  </Tooltip>
-                </span>
-                {element.stargazers_count}
-              </Typography>
-              <Typography display="inline">
-                <span>
-                  <Tooltip title="Forks" placement="top">
-                    <img
-                      src={gitfork}
-                      style={{
-                        verticalAlign: "sub",
-                        fontSize: "1.25em",
-                        marginLeft: "10px",
-                        marginRight: "5px",
-                      }}
-                    />
-                  </Tooltip>
-                </span>
-                {element.forks_count}
-              </Typography>
+                  <Typography display="inline">
+                    <span>
+                      <Tooltip title="Forks" placement="top">
+                        <span
+                          style={{
+                            fontSize: "1.25em",
+                            marginLeft: "10px",
+                            marginRight: "5px",
+                          }}
+                        >
+                          <Octicon icon={getIconByName("repo-forked")} />
+                        </span>
+                      </Tooltip>
+                    </span>
+                    {element.forks_count}
+                  </Typography>
+                </div>
+                <div>
+                  <Typography component={"span"}>
+                    {numberWithCommas(element.size)} KB
+                  </Typography>
+                </div>
+              </div>
             </Paper>
           </a>
         </Grid>
